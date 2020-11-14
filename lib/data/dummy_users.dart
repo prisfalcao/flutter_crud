027f555af26278081eb941263ef12303908e3d9c
Priscila Falcão <falcao.priscila@hotmail.com>
2020-11-14 16:39:55 -0300

    Flutter Crud

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Joyce',
+    email: 'joyce@tibia.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2014/04/02/14/10/female-306407__480.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Priscila',
+    email: 'priscila@tibia.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387__480.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Pudol',
+    email: 'pudol@tibia.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2019/02/06/15/18/puppy-3979350__480.jpg',
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Elvis',
+    email: 'elvis@tibia.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2015/01/28/14/33/cockatiel-614905__480.jpg',
+  ),
+};
